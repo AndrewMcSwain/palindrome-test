fe0b65d158023ddd7b363ecfe7fb15fb84a205d9
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-18 00:41:48 -0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,22 @@
+function palindrome(str) {
+  function processStr(str){
+    str.toLowerCase();
+    //remove non-alphanumeric characters
+  }
+  
+  var strRev = function(str){
+    var array = str.split("");
+    array.reverse();
+    return array.join("");
+  };
+  
+  
+  if(strRev == str){
+  return true;}
+}
+
+
+
+palindrome("eye");
+
+  
