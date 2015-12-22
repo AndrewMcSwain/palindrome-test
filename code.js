a582eba703c8acbd6dea471451fa600660aa24df
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-22 21:18:16 +0000

    Update code.js
    added new function removeChars. need to use a different property of string type. split is for arrays.

--- code.js
+++ code.js
@@ -1,8 +1,24 @@
 function palindrome(str) {
+  
+  var stringLowerCase = "";
+  
   function processStr(str){
-    str.toLowerCase();
+    stringLowerCase = str.toLowerCase();
+    return stringLowerCase;
     //remove non-alphanumeric characters
   }
+
+  
+  function removeChars(stringLowerCase, str){
+     for(i=0; i < str.length; i++){
+       index = stringLowerCase.indexOf(i);
+       value = stringLowerCase.getValue(index);
+       if (value != /\w/){
+         strLowerCase.split(index);
+       }
+   }
+  }
+  
   
   var strRev = function(str){
     var array = str.split("");
@@ -10,6 +26,13 @@ function palindrome(str) {
     return array.join("");
   };
   
+  return true;
+}
+
+
+
+palindrome("eye");
+
   
   if(strRev == str){
   return true;}
