f6c5c3eab640e87f4d6a62cea5be04c1a8ae6765
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-25 10:10:41 -0200

    Update code.js
    Added note at the end

--- code.js
+++ code.js
@@ -43,3 +43,4 @@ palindrome("eye");
 palindrome("eye");
 
   
+//in production version change to array and reverse it, then run removeChar process on reversed array then replace str with unreversed and processed version, then compare to see if it's a palindrome.
