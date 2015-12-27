a40a2fd81627ef17a32beaf77edb7d909e6db31a
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-26 23:46:54 -0200

    Update code.js
    cleaned it up and made it readable. made each step a variable with a function rather than a function that produces a variable.

--- code.js
+++ code.js
@@ -1,48 +1,29 @@
 function palindrome(str) {
-  
-  var stringLowerCase = "";
-  var alphaString = "";
+ 
+  //make the string lower case
+  var stringLowerCase = function processStr(){
+    stringLowerCase = str.toLowerCase();
+    return stringLowerCase;
+    };
+  //remove all non alpha characters
+  var alphaString = function removeChars(str){
+    var remArray = []; 
+    for(i=0; i < stringLowerCase.length; i++){
+      index = i;
+      value = stringLowerCase.charAt(index); 
+      if (value !== /\w/ === true){
+        remArray.push(index);
+       }
+    }	
+    for(i=0; i < remArray.length; i++){
+      alphaString = stringLowerCase.split(remArray[i]);
+    }       
+  };
+  //reverse the formatted string
   var strRev = function(){
     var array = alphaString.split("");
     array.reverse();
     return array.join("");
   };
   
-  function processStr(){
-    stringLowerCase = str.toLowerCase();
-    return stringLowerCase;
-    //remove non-alphanumeric characters
-  }
-  
-  
-  function removeChars(str){
-    var remArray = []; 
-    for(i=0; i < str.length; i++){
-       index = i;
-       value = stringLowerCase.charAt(index); //change getValue() to charAt()
-       if (value != /\w/){
-           remArray.push(index);
-       		}
-     }	
-     for(i=0; i < remArray.length; i++){
-     	alphaString = stringLowerCase.split(remArray[i]);
-     }       
-}
-
-  if (strRev == str){
-  return true;
-  }  
-  
-  processStr(str);  
-  removeChars(stringLowerCase, str);
-}
-
-
-
 palindrome("eye");
-
-  
-
-
-
-
