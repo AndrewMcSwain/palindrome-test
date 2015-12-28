03d81548cb6e13dd8f8f9732349bf964256539eb
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-28 18:30:30 +0000

    small syntax changes
    removed var from "i" variables in for loop.
    added a closing }t to the function palindrome
    added alphaString as parametre to function called by stRev

--- code.js
+++ code.js
@@ -9,28 +9,29 @@ function palindrome(str) {
   //remove all non alpha characters
   var alphaString = function removeChars(stringLowerCase){
     var remArray = []; 
-    for(var i=0; i < stringLowerCase.length; i++){
+    for(i=0; i < stringLowerCase.length; i++){
       var index = i;
       var value = stringLowerCase.charAt(index); 
       if (value !== /\w/ === true){
         remArray.push(index);
        }
     }	
-    for(var i=0; i < remArray.length; i++){
+    for(i=0; i < remArray.length; i++){
       alphaString = stringLowerCase.split(remArray[i]);
     }       
   };
   //reverse the formatted string
-  var strRev = function(){
+  var strRev = function(alphaString){
     var array = alphaString.split("");
     array.reverse();
     return array.join("");
   };
  
-  if (alphaString = strRev === true)
+  if (alphaString == strRev === true)
  	return true;
   else return false;
-  
+ 
+}
   //need to compare strRev to str and if they are the same return true.
   
 palindrome("eye");
