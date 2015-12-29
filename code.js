8edd39db9427fb5cf39111a914c426ab4665eaf5
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-29 19:25:51 +0000

    Update code.js
    completely reworked it

--- code.js
+++ code.js
@@ -1,25 +1,41 @@
 function palindrome(str) {
-    //lower case it
+//lower case it
     var stringLowerCase = str.toLowerCase();
-    //remove all non-alphanumeric characters without .replace
+//remove all non-alphanumeric characters without .replace
     var alphaNumeric = function(){
       for(i = 0; i < stringLowerCase.lengh; i++){
         var alphaNumericChar = stringLowerCase.charAt(i);
         var alphaNumericIndex = i;
-        var removeArray = [];
-      //find the unwanted charactes
+        removeArray = [];
+//find the unwanted charactes
         if (alphaNumericChar != /\w/ || /\d/){
           removeArray.push(alphaNumericIndex);
           }
         return removeArray;
       } 
-      //remove the unwanted characters
+//remove the unwanted characters
       for(i = 0; i < stringLowerCase.length; i++){
-        var alphaNumericSting = stringLowerCase.split(removeArray[i]);
-        //may need to subtract one(1) from the following item on removeArray each time the for         loop runs.
-       // removeArray.forEach(/*subtract 1*/);
-        
-      }
-    }
-};
+        var alphaNumericSting = stringLowerCase.slice(removeArray[i],removeArray[i]);
+        removeArray = removeArray.map(function(num) {return num - 1;});  
+       }
+    return alphaNumericString;
+    };
+//reverse the string cleaned of unwanted characters  
+  var revAlphaNumeric = function(){
+    var alphaNumericArray = alphaNumericString.split(" ");
+    var reverseAlphaNumericArray = alphaNumericArray.reverse();
+    var reverseAlphaNumericString = reverseAlphaNumericArray.join();
+    return reverseAlphaNumericString;
+  };
+  
+  function compare(string, revstring){
+    if (string == revstring)
+      return true;
+    else
+      return false;
+  }
+  
+  return compare(alphaNumeric, revAlphaNumeric);
+ 
+}
 palindrome("WHAT");
