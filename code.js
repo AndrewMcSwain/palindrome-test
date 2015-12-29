4f167c20a72321f814b46ea7636846abdeb62962
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-29 22:01:46 +0000

    Update code.js
    removed unnecessary functions and modularization

--- code.js
+++ code.js
@@ -2,42 +2,35 @@ function palindrome(str) {
 //lower case it
     var stringLowerCase = str.toLowerCase();
 //remove all non-alphanumeric characters without .replace
-    var alphaNumeric = function(){
-      var removeArray = [];  
-      var alphaNumericString = "";
-      for(i = 0; i < stringLowerCase.length; i++){
-        var alphaNumericChar = stringLowerCase.charAt(i);
-        var alphaNumericIndex = i;
-        
-//find the unwanted charactes
-        if (alphaNumericChar != /\w/ || /\d/ || /\s/){
+    var removeArray = [];  
+    var alphaNumericString = "";
+//find the unwanted characters
+    for(i = 0; i < stringLowerCase.length; i++){
+      var alphaNumericChar = stringLowerCase.charAt(i);
+      var alphaNumericIndex = i;
+      if (alphaNumericChar != /\w/ || /\d/ || /\s/){
           removeArray.push(alphaNumericIndex);
           }
-        return removeArray;
+      return removeArray;
       } 
 //remove the unwanted characters
       for(i = 0; i < stringLowerCase.length; i++){
-        alphaNumericString = stringLowerCase.slice(removeArray[i],removeArray[i]);
+        var alphaNumericString = stringLowerCase.slice(removeArray[i], removeArray[i]);
         removeArray = removeArray.map(function(num) {return num - 1;});  
        }
-    return alphaNumericString;
-    };
 //reverse the string cleaned of unwanted characters  
-  var revAlphaNumeric = function(){
-    var alphaNumericArray = alphaNumeric().split(" ");
+    var alphaNumericArray = alphaNumericString.split("");
     var reverseAlphaNumericArray = alphaNumericArray.reverse();
     var reverseAlphaNumericString = reverseAlphaNumericArray.join();
-    return reverseAlphaNumericString;
-  };
-
+//compare the value of two strings
   function compare(string, revstring){
-    if (string == revstring)
+    if (string === revstring)
       return true;
     else
       return false;
   }
-  
-  return compare(alphaNumeric(), revAlphaNumeric());
  
+  return compare(alpha(), revAlphaNumeric());
+
 }
-palindrome("not a palindrome");
+palindrome("eye");
