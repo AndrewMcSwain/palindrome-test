95daa4ebdd3a4fa23d372035889b8a56af31c7d7
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2015-12-31 02:26:32 +0000

    Update code.js
    using a map inside a for loop to subtract 1 from each item in the array

--- code.js
+++ code.js
@@ -1,37 +1,12 @@
-function palindrome(str) {
-//lower case it
-    var stringLowerCase = str.toLowerCase();
-//remove all non-alphanumeric characters without .replace
-    var removeArray = [];  
-    var alphaNumericString = "";
-//find the unwanted characters
-    for(i = 0; i < stringLowerCase.length; i++){
-      var alphaNumericChar = stringLowerCase.charAt(i);
-      var alphaNumericIndex = i;
-      if (alphaNumericChar != /\w/ || /\d/ || /\s/){
-          removeArray.push(alphaNumericIndex);
-          }
-      } 
-//remove the unwanted characters
-      for(i = 0; i < removeArray.length; i++){
-        alphaNumericString = stringLowerCase.slice(removeArray[i], removeArray[i]);
-        removeArray = removeArray.map(function(num) {return num - 1;});  
+function map(){
+  removeArray = [4, 4, 4];
+   for(i = 0; i < removeArray.length; i++){
+       removeArray = removeArray.map(
+        function(num){
+            return (num - 1);
+            }
+         );  
        }
-//reverse the string cleaned of unwanted characters  
-    var alphaNumericArray = alphaNumericString.split("");
-    var reverseAlphaNumericArray = alphaNumericArray.reverse();
-    var reverseAlphaNumericString = reverseAlphaNumericArray.join("");
-//compare the value of two strings
-  function compare(string, revstring){
-    if (string == revstring)
-      return true;
-    else
-      return false;
-  }
- 
-  return compare(alphaNumericString, reverseAlphaNumericString);
-
+  return removeArray;
 }
-palindrome("eye");
-palindrome("almoststomla");
-palindrome("E1YE");
+map();
