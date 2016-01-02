89c1bb99f69cc6ea601bd1935eeb9c7b4a01e133
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-02 21:06:12 -0200

    Update code.js
    fixed second parameter of second slice (line 8), was pointing to string.length. now it's array.length.
    added stringLength function-wide variable (line 3)
    changed l = array.length to arrayLength = arrayLength (line 2)

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 function cleanString(string, array){
-  var l = array.length;
+  var arrayLength = array.length;
+  var stringLength = string.length;
   var concatenated = "";
   for(var i = 0; i < l; i++){
   	 var beg = string.slice(0, array[i]);
      var next = array[i] + 1;
-     var end = string.slice(next, l);
+     var end = string.slice(next, stringLength);
   	 concatenated = beg + end;
   }
   console.log(concatenated);
