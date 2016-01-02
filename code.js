99534458085f9e59e1def2c4b5994f03c185ba7e
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-02 17:27:49 -0200

    Update code.js
    functional removeArray... used the method `test`

--- code.js
+++ code.js
@@ -1,37 +1,17 @@
-function palindrome(str) {
-//lower case it
-    var stringLowerCase = str.toLowerCase();
-//remove all non-alphanumeric characters without .replace
-    var removeArray = [];  
-    var alphaNumericString = "";
-//find the unwanted characters
-    for(i = 0; i < stringLowerCase.length; i++){
-      var alphaNumericChar = stringLowerCase.charAt(i);
-      var alphaNumericIndex = i;
-      if (alphaNumericChar != /\w/ || /\d/ || /\s/){
-          removeArray.push(alphaNumericIndex);
-          }
-      } 
-//remove the unwanted characters
-      for(i = 0; i < removeArray.length; i++){
-        alphaNumericString = stringLowerCase.slice(removeArray[i], removeArray[i]);
-        removeArray = removeArray.map(function(num) {return num - 1;});  
-       }
-//reverse the string cleaned of unwanted characters  
-    var alphaNumericArray = alphaNumericString.split("");
-    var reverseAlphaNumericArray = alphaNumericArray.reverse();
-    var reverseAlphaNumericString = reverseAlphaNumericArray.join("");
-//compare the value of two strings
-  function compare(string, revstring){
-    if (string == revstring)
-      return true;
-    else
-      return false;
-  }
- 
-  return compare(alphaNumericString, reverseAlphaNumericString);
-
+function palindrome(str){
+stringLowerCase = str.toLowerCase();
+removeArray = [];
+for(i = 0; i < stringLowerCase.length; i++){
+  var alphaNumericChar = stringLowerCase.charAt(i);
+  var alphaNumericIndex = i;
+  var isLetter = /\w/.test(alphaNumericChar);
+  var isNumber = /\d/.test(alphaNumericChar);
+  if (isLetter === false && isNumber === false){
+    	removeArray.push(alphaNumericIndex);
+      }
+   	}
+return console.log(removeArray);
+  
 }
-palindrome("eye");
-palindrome("almoststomla");
-palindrome("E1YE");
+
+palindrome("this is the master race of gerbil fuckers");
