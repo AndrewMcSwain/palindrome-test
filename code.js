1dea615cd8f373452675e8ae972f6c8c7da9280c
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-02 21:30:41 -0200

    Update code.js
    added testing
    changed next from array[i] +1 to array[i]

--- code.js
+++ code.js
@@ -3,15 +3,20 @@ function cleanString(string, array){
   var stringLength = string.length;
   var concatenated = "";
   for(var i = 0; i < arrayLength; i++){
+     console.log("array: " + array[i]);
   	 var beg = string.slice(0, array[i]);
-     var next = array[i] + 1;
+     console.log("beg: " + beg);
+     var next = array[i]
+     console.log("next: " + next);
      var end = string.slice(next, stringLength);
-     console.log(end);
+     console.log("end: " + end);
   	 concatenated = beg + end;
      /*for(var a = 0; a < arrayLength; i++){
        array[a] = array[a]-1;*/
      }
      
  
-  console.log(concatenated);
+  console.log("concatenated is: " + concatenated);
 }
+
+cleanString("this is the master race of gerbils", [1,3]);
