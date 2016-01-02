f43dc358d2d1e6989197cac7707141999c53c50a
Vishnu Airy (Aero-vision) <andrew@e4b.us>
2016-01-02 21:23:51 -0200

    Update code.js
    added console.log after var end to see whats going on
    added (commented out) for loop to remove value from the array, if needed.

--- code.js
+++ code.js
@@ -2,13 +2,16 @@ function cleanString(string, array){
   var arrayLength = array.length;
   var stringLength = string.length;
   var concatenated = "";
-  for(var i = 0; i < l; i++){
+  for(var i = 0; i < arrayLength; i++){
   	 var beg = string.slice(0, array[i]);
-     var next = i + 1;
+     var next = array[i] + 1;
      var end = string.slice(next, stringLength);
+     console.log(end);
   	 concatenated = beg + end;
-  }
+     /*for(var a = 0; a < arrayLength; i++){
+       array[a] = array[a]-1;*/
+     }
+     
+ 
   console.log(concatenated);
 }
-
-cleanString("this is the master race of gerbils", [3,5,7,10]);
